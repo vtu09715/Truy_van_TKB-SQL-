281887d8af521b33093a3abe0f58f62ac9240dbb
vtu09715 <163267453+vtu09715@users.noreply.github.com>
2025-04-14 10:37:57 +0700

    Add files via upload

---
+++ bai_tap_4_data.sql
@@ -0,0 +1,17 @@
+﻿DECLARE @datetime1 DATETIME = '2025-03-17 06:30:00';
+DECLARE @datetime2 DATETIME = '2025-03-18 09:10:00';
+
+SELECT DISTINCT
+    GV.HoTen AS N'Họ tên GV',
+    MH.TenMH AS N'Môn dạy',
+    LHP.Tenlop AS N'Lớp học',
+    TKB.PhongHoc AS N'Phòng học',
+    TKB.GioVao AS N'Giờ vào',
+    TKB.GioRa AS N'Giờ ra'
+FROM dbo.TKB
+JOIN dbo.GiaoVien GV ON TKB.MaGV = GV.MaGV
+JOIN dbo.MonHoc MH ON TKB.MaMH = MH.MaMH
+JOIN dbo.LopHocPhan LHP ON TKB.MaLHP = LHP.MaLHP
+WHERE
+    CAST(TKB.NgayHoc AS DATETIME) + CAST(TKB.GioRa AS DATETIME) > @datetime1 AND
+    CAST(TKB.NgayHoc AS DATETIME) + CAST(TKB.GioVao AS DATETIME) < @datetime2
